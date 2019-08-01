060786e05c1822cb99fb399b205e5ed5b54414c4
RocktimSaikia <srocktim61@gmail.com>
2019-08-01 11:18:52 -0700

    added link to more button

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -43,59 +43,56 @@ const HomePage = props => {
               <Grid item xs={12} sm={6} key={index}>
                 <Fade bottom cascade duration={1500}>
                   <Paper className={classes.paper}>
-                    <a
-                      href={canonicalVolumeLink}
-                      target="_blank"
-                      style={{ textDecoration: "none", color: "#111" }}
-                      className="link"
-                    >
-                      <Grid container spacing={2}>
-                        <Grid item>
-                          <img
-                            src={thumbnail}
-                            alt="just an image"
-                            height="180px"
-                            width="150px"
-                          />
-                          <div className="stars">
-                            <i className="fas fa-star" />
-                            <i className="fas fa-star" />
-                            <i className="fas fa-star" />
-                            <i className="fas fa-star" />
-                            <i className="fas fa-star" />
-                          </div>
-                        </Grid>
-                        <Grid item container xs direction="column">
-                          <Typography align="left">
-                            <h2>
-                              {title}
-                              <span className="genre">
-                                <i className="fas fa-book">{categories}</i>
-                              </span>
-                            </h2>
-                            <h5>by {authors}</h5>
-                            <p style={{ marginBottom: "10px" }}>
-                              {publisher} <br />
-                              Pub. Date: {publishedDate} <br />
-                              {industryIdentifiers[0].type}:{" "}
-                              {industryIdentifiers[0].identifier}
-                              <br />
-                              {industryIdentifiers[1]
-                                ? industryIdentifiers[1].type
-                                : ""}
-                              {industryIdentifiers[1]
-                                ? industryIdentifiers[1].identifier
-                                : ""}
-                            </p>
-                            <a href="" className="more">
-                              More
-                            </a>
-
-                            {/* <p>{formatDescription(searchInfo.textSnippet)}</p> */}
-                          </Typography>
-                        </Grid>
+                    <Grid container spacing={2}>
+                      <Grid item>
+                        <img
+                          src={thumbnail}
+                          alt="just an image"
+                          height="180px"
+                          width="150px"
+                        />
+                        <div className="stars">
+                          <i className="fas fa-star" />
+                          <i className="fas fa-star" />
+                          <i className="fas fa-star" />
+                          <i className="fas fa-star" />
+                          <i className="fas fa-star" />
+                        </div>
                       </Grid>
-                    </a>
+                      <Grid item container xs direction="column">
+                        <Typography align="left">
+                          <h2>
+                            {title}
+                            <span className="genre">
+                              <i className="fas fa-book">{categories}</i>
+                            </span>
+                          </h2>
+                          <h5>by {authors}</h5>
+                          <p style={{ marginBottom: "10px" }}>
+                            {publisher} <br />
+                            Pub. Date: {publishedDate} <br />
+                            {industryIdentifiers[0].type}:{" "}
+                            {industryIdentifiers[0].identifier}
+                            <br />
+                            {industryIdentifiers[1]
+                              ? industryIdentifiers[1].type
+                              : ""}
+                            {industryIdentifiers[1]
+                              ? industryIdentifiers[1].identifier
+                              : ""}
+                          </p>
+                          <a
+                            href={canonicalVolumeLink}
+                            target="_blank"
+                            className="more"
+                          >
+                            More
+                          </a>
+
+                          {/* <p>{formatDescription(searchInfo.textSnippet)}</p> */}
+                        </Typography>
+                      </Grid>
+                    </Grid>
                   </Paper>
                 </Fade>
               </Grid>
